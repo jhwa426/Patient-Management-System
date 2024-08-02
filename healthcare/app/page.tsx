cdfeb9c052f7bd1ec5d214c9bc83026cfcf0d22d
Jeff Hwang <wodud6359@gmail.com>
2024-08-02 15:07:19 +1200

    Homepage

--- healthcare/app/page.tsx
+++ healthcare/app/page.tsx
@@ -1,10 +1,23 @@
-import { Button } from "@/components/ui/button";
+import { PatientForm } from "@/components/forms/PatientForm";
+import Image from "next/image";
 
 export default function Home() {
     return (
-        <div>
-            <h1 className="text-white text-3xl underline">Home</h1>
-            <Button>Click me</Button>
+        <div className="flex h-screen max-h-screen">
+            <section className="remove-scrollbar container my-auto">
+                <div className="sub-container max-w-[496px]">
+                    <Image
+                        src="/assets/icons/logo-full.svg"
+                        height={1000}
+                        width={1000}
+                        alt="patient"
+                        className="mb-12 h-10 w-fit"
+                    />
+
+                    <PatientForm />
+                </div>
+            </section>
+
         </div>
     );
 }
---
+++ healthcare/components/forms/PatientForm.tsx
@@ -0,0 +1,10 @@
+
+
+export const PatientForm = () => {
+    return (
+        <div>
+            PatientForm
+        </div>
+    );
+}
+
