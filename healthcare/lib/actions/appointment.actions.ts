fb5fb9d7bbc84567bd337abd3c080310d405e931
Jeff Hwang <wodud6359@gmail.com>
2024-08-09 12:40:44 +1200

    success-page

--- healthcare/lib/actions/appointment.actions.ts
+++ healthcare/lib/actions/appointment.actions.ts
@@ -25,9 +25,30 @@ export const createAppointment = async (appointmentData: CreateAppointmentParams
         );
 
         revalidatePath("/admin");
+
         return parseStringify(newAppointment);
 
     } catch (error) {
         console.error("An error occurred while creating a new appointment:", error);
+    }
+}
+
+
+// GET APPOINTMENT
+export const getAppointment = async (appointmentId: string) => {
+    try {
+        const appointment = await databases.getDocument(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            appointmentId
+        );
+
+        return parseStringify(appointment);
+
+    } catch (error) {
+        console.error(
+            "An error occurred while retrieving the existing patient:",
+            error
+        );
     }
 }
